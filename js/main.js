671f5ba09cdb983b697e211d91a49c1d4c167d18
Jeroen-TheCodingFactory <tcf@jeroen-rijsdijk.nl>
2023-02-16 09:40:40 +0100

    first part of refactor to oop

--- js/main.js
+++ js/main.js
@@ -1,26 +1,24 @@
-const colors = document.getElementsByClassName("colors__color");
+class ColorCard{
+    id;
+    color;
+    addToList;
 
-/* Als laatste wil je geen 
-kleuren die te donker zijn (<= 10%) 
-en geen kleuren die te kleur bevatten (<= 10%) 
-en teveel kleur (>= 80%) bevatten.  
- */
-for(let i = 0; i < colors.length; i++ ){
-    //nummer inclusief 1 - 360 inclusief => hue
-    //percentage inclusief 11 - 79 => saturatie
-    //percentage inclusief 11 - 100 => licht
-    //x = Math.random() * (max - min) + min;
+    constructor(newId, newColor, addToList){
+        this.id = newId;
+        this.color = newColor;
+        this.addToList = addToList;
+    }
 
-    // colors[i].style.animationDelay = i/10 + "s";
-    let randomHue = Math.floor(Math.random() * (360 - 1) + 1);
-    let randomSaturation = Math.floor(Math.random() * (79 - 11) + 11) + "%";
-    let randomLightness = Math.floor(Math.random() * (100 - 11) + 11) + "%";
-
-    colors[i].children[0].style.background = `hsl(${randomHue} ${randomSaturation} ${randomLightness})`;
-    /* onclick */
-    colors[i].onclick = function(){
-        colors[i].children[0].classList.add("colors__circle--selected");
-        navigator.clipboard.writeText(colors[i].children[0].style.background);
-        document.title = colors[i].children[0].style.background;
+    render(){
+        const listItemToBeRendered = document.createElement("li");
+        listItemToBeRendered.classList = "colors__color";
+        const figureToBeRendered = document.createElement("figure");
+        figureToBeRendered.classList = "colors__circle";
+        figureToBeRendered.style.background = this.color;
+        listItemToBeRendered.appendChild(figureToBeRendered);
+        this.addToList.appendChild(listItemToBeRendered);
     }
 }
+
+const test = new ColorCard(101, "rgba(255,0,0,1)",document.getElementById("js--colors"));
+test.render();
