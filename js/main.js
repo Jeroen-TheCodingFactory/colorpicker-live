80dddf438ec494697c12092793c0a64a0c0f521c
Jeroen-TheCodingFactory <tcf@jeroen-rijsdijk.nl>
2023-02-16 10:16:57 +0100

    completed first version of refactor

--- js/main.js
+++ js/main.js
@@ -2,23 +2,54 @@ class ColorCard{
     id;
     color;
     addToList;
+    htmlElement;
+    circle;
+    text;
 
     constructor(newId, newColor, addToList){
+        /*setting properties */
         this.id = newId;
         this.color = newColor;
         this.addToList = addToList;
+
+        /* make htmlElement to render */
+        this.htmlElement = document.createElement("li");
+        this.htmlElement.classList = "colors__color";
+
+        this.circle = document.createElement("figure");
+        this.circle.classList = "colors__circle";
+        this.circle.style.background = this.color;
+       
+        this.text = document.createElement("p");
+        this.text.innerText = "Copied";
+        this.text.classList = "colors__text";
+ 
+        this.htmlElement.onclick = this.onHTMLElementClicked;
+        /* finally render */
+        this.render();
+
+    }
+
+    onHTMLElementClicked = () =>{
+        this.circle.classList.add("colors__circle--selected");
+        document.title = this.color;
+        window.navigator.clipboard.writeText(this.color);
     }
 
     render(){
-        const listItemToBeRendered = document.createElement("li");
-        listItemToBeRendered.classList = "colors__color";
-        const figureToBeRendered = document.createElement("figure");
-        figureToBeRendered.classList = "colors__circle";
-        figureToBeRendered.style.background = this.color;
-        listItemToBeRendered.appendChild(figureToBeRendered);
-        this.addToList.appendChild(listItemToBeRendered);
+        this.htmlElement.appendChild(this.circle);
+        this.htmlElement.appendChild(this.text);
+        this.addToList.appendChild(this.htmlElement);
     }
 }
 
-const test = new ColorCard(101, "rgba(255,0,0,1)",document.getElementById("js--colors"));
-test.render();
+for(let i = 1; i < 101; i++ ){
+
+    // colors[i].style.animationDelay = i/10 + "s";
+    let randomHue = Math.floor(Math.random() * (360 - 1) + 1);
+    let randomSaturation = Math.floor(Math.random() * (79 - 11) + 11) + "%";
+    let randomLightness = Math.floor(Math.random() * (100 - 11) + 11) + "%";
+
+    let hsl = `hsl(${randomHue}, ${randomSaturation}, ${randomLightness})`
+    new ColorCard(i, hsl ,document.getElementById("js--colors"));
+}
